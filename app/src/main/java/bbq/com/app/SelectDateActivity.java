e469740944ec811b28d58957e7df8103f23ba44a
Gaurav0812 <gaurav7870>
2017-04-26 19:25:12 +0530

    1st Commit

---
+++ app/src/main/java/bbq/com/app/SelectDateActivity.java
@@ -0,0 +1,13 @@
+package bbq.com.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectDateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_date);
+    }
+}
